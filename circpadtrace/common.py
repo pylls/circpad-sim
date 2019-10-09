34ae198298c9a3ed4366680a0aa4d0db574cf4b3
pulls <tobias.pulls@kau.se>
2019-10-09 17:19:38 +0200

    basic bandwidth overhead script

--- circpadtrace/common.py
+++ circpadtrace/common.py
@@ -4,6 +4,11 @@ import sys
 CIRCPAD_ERROR_WRONG_FORMAT = "invalid trace format"
 CIRCPAD_ADDRESS_EVENT = "connection_ap_handshake_send_begin"
 
+CIRCPAD_EVENT_NONPADDING_SENT = "circpad_cell_event_nonpadding_sent"
+CIRCPAD_EVENT_NONPADDING_RECV = "circpad_cell_event_nonpadding_received"
+CIRCPAD_EVENT_PADDING_SENT = "circpad_cell_event_padding_sent"
+CIRCPAD_EVENT_PADDING_RECV = "circpad_cell_event_padding_received"
+
 def circpad_get_all_addresses(trace):
     addresses = []
     for l in trace:
@@ -14,3 +19,27 @@ def circpad_get_all_addresses(trace):
                 sys.exit(CIRCPAD_ERROR_WRONG_FORMAT)
             addresses.append(l[1].split()[1])
     return addresses
+
+def circpad_get_nonpadding_times(trace):
+    sent_nonpadding, recv_nonpadding = [], []
+
+    for l in trace:
+        split = l.split()
+        if CIRCPAD_EVENT_NONPADDING_SENT in split[1]:
+            sent_nonpadding.append(split[0])
+        elif CIRCPAD_EVENT_NONPADDING_RECV in split[1]:
+            recv_nonpadding.append(split[0])
+    
+    return sent_nonpadding, recv_nonpadding
+
+def circpad_get_padding_times(trace):
+    sent_padding, recv_padding = [], []
+
+    for l in trace:
+        split = l.split()
+        if CIRCPAD_EVENT_PADDING_SENT in split[1]:
+            sent_padding.append(split[0])
+        elif CIRCPAD_EVENT_PADDING_RECV in split[1]:
+            recv_padding.append(split[0])
+    
+    return sent_padding, recv_padding

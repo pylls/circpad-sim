67730389f7a4001e470c5170b51caa1f616467f6
pulls <tobias.pulls@kau.se>
2019-11-12 15:35:15 +0100

    refactored out the trace extraction from a log into common

--- common.py
+++ common.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import socket
 
 CIRCPAD_ERROR_WRONG_FORMAT = "invalid trace format"
 CIRCPAD_ADDRESS_EVENT = "connection_ap_handshake_send_begin"
@@ -8,6 +9,20 @@ CIRCPAD_EVENT_NONPADDING_SENT = "circpad_cell_event_nonpadding_sent"
 CIRCPAD_EVENT_NONPADDING_RECV = "circpad_cell_event_nonpadding_received"
 CIRCPAD_EVENT_PADDING_SENT = "circpad_cell_event_padding_sent"
 CIRCPAD_EVENT_PADDING_RECV = "circpad_cell_event_padding_received"
+
+CIRCPAD_LOG = "circpad_trace_event"
+CIRCPAD_LOG_TIMESTAMP = "timestamp="
+CIRCPAD_LOG_CIRC_ID = "client_circ_id="
+CIRCPAD_LOG_EVENT = "event="
+
+CIRCPAD_BLACKLISTED_ADDRESSES = ["aus1.torproject.org"]
+CIRCPAD_BLACKLISTED_EVENTS = [
+    "circpad_negotiate_padding", 
+    "circpad_handle_padding_negotiated", 
+    "circpad_handle_padding_negotiate",
+    "circpad_padding_negotiated"
+]
+
 
 def circpad_get_all_addresses(trace):
     addresses = []
@@ -51,3 +66,116 @@ def circpad_parse_line(line):
     timestamp = int(split[0])
 
     return event, timestamp
+
+def circpad_extract_log_traces(
+    log_file, 
+    allow_ips=False, 
+    filter_client_negotiate=False,
+    filter_relay_negotiate=False
+    ):
+    # helper function
+    def blacklist_hit(d):
+        for a in circpad_get_all_addresses(d):
+            if a in CIRCPAD_BLACKLISTED_ADDRESSES:
+                return True
+        return False
+
+    # helper to extract one line
+    def extract_from_line(line):
+        n = line.index(CIRCPAD_LOG_TIMESTAMP)+len(CIRCPAD_LOG_TIMESTAMP)
+        timestamp = line[n:].split(" ", maxsplit=1)[0]
+        n = line.index(CIRCPAD_LOG_CIRC_ID)+len(CIRCPAD_LOG_CIRC_ID)
+        cid = line[n:].split(" ", maxsplit=1)[0]
+
+        # an event is the last part, no need to split on space like we did earlier
+        n = line.index(CIRCPAD_LOG_EVENT)+len(CIRCPAD_LOG_EVENT)
+        event = line[n:]
+        
+        return cid, timestamp, event
+
+    circuits = {}
+    with open(log_file, 'r') as f:
+        for line in f:
+            if CIRCPAD_LOG in line:
+                cid, timestamp, event = extract_from_line(line)
+                if cid in circuits.keys():
+                    circuits[cid] = circuits.get(cid) + [(timestamp, event)]
+                else:
+                    circuits[cid] = [(timestamp, event)]
+
+    # filter out circuits with blacklisted addresses
+    for cid in list(circuits.keys()):
+        if blacklist_hit(circuits[cid]):
+            del circuits[cid]
+    # filter out circuits with only IPs (unless arg says otherwise)
+    for cid in list(circuits.keys()):
+        if not allow_ips and circpad_only_ips_in_trace(circuits[cid]):
+            del circuits[cid]
+
+    # remove blacklisted events (and associated events)
+    for cid in list(circuits.keys()):
+        circuits[cid] = circpad_remove_blacklisted_events(circuits[cid],
+                        filter_client_negotiate, filter_relay_negotiate)
+    
+    return circuits
+    
+
+def circpad_remove_blacklisted_events(
+    trace, 
+    filter_client_negotiate, 
+    filter_relay_negotiate
+    ):
+    
+    # three cases: no argument, filter client, or filter relay
+    result = []
+    ignore_next_events = 0
+
+    for line in trace:
+        # we always filter blacklisted events
+        if not any(b in line for b in CIRCPAD_BLACKLISTED_EVENTS):
+            if ignore_next_events == 0:
+                result.append(line)
+            else:
+                ignore_next_events -= 1
+
+    '''
+    For the client, we assume a list as follows:
+      1. circpad_negotiate_padding
+      2. circpad_cell_event_nonpadding_sent
+      3. circpad_cell_event_nonpadding_received
+      4. circpad_handle_padding_negotiated
+    On observing (blacklisted) event 1, we ignore the following 2 events
+    '''
+    if filter_client_negotiate and "circpad_negotiate_padding" in line:
+        ignore_next_events = 2
+
+    '''
+    For the relay, we assume a list as followes:
+      1. circpad_handle_padding_negotiate
+      2. circpad_padding_negotiated
+      3. circpad_cell_event_nonpadding_sent
+    On observing (blacklisted) event 2, we ignore the following 1 event
+    '''
+    if filter_relay_negotiate and "circpad_padding_negotiated" in line:
+        ignore_next_events = 1
+
+    return result
+
+def circpad_only_ips_in_trace(trace):
+    def is_ipv4(addr):
+        try:
+            socket.inet_aton(addr)
+        except (socket.error, TypeError):
+            return False
+        return True
+    def is_ipv6(addr):
+        try:
+            socket.inet_pton(addr,socket.AF_INET6)
+        except (socket.error, TypeError):
+            return False
+        return True
+
+    for a in circpad_get_all_addresses(trace):
+        if not is_ipv4(a) and not is_ipv6(a):
+            return False
+    return True
--- torlog2circpadtrace.py
+++ torlog2circpadtrace.py
@@ -2,7 +2,7 @@
 import argparse
 import os
 import sys
-import socket
+
 import common
 
 ap = argparse.ArgumentParser()
@@ -21,18 +21,6 @@ ap.add_argument('--fnr', default=False, action='store_true',
 args = vars(ap.parse_args())
 
 CONST_OTHER_MAX_EVENTS_WARNING = 100
-CONST_CIRCPAD_TRACE = "circpad_trace_event"
-CONST_CIRCPAD_TRACE_TIMESTAMP = "timestamp="
-CONST_CIRCPAD_TRACE_CIRC_ID = "client_circ_id="
-CONST_CIRCPAD_TRACE_EVENT = "event="
-
-blacklisted_addresses = ["aus1.torproject.org"]
-blacklisted_events = [
-    "circpad_negotiate_padding", 
-    "circpad_handle_padding_negotiated", 
-    "circpad_handle_padding_negotiate",
-    "circpad_padding_negotiated"
-]
 
 def main():
     '''Given an input folder of logs from instrumented tor to log circuitpad traces 
@@ -56,31 +44,10 @@ def main():
             sys.exit(f"output file {outfname} already exists")
 
         # create dictionary with circuit_id -> [(timestamp, event)]
-        circuits = {}
-        with open(infname, 'r') as f:
-            for line in f:
-                if CONST_CIRCPAD_TRACE in line:
-                    cid, timestamp, event = extract_trace(line)
-                    if cid in circuits.keys():
-                        circuits[cid] = circuits.get(cid) + [(timestamp, event)]
-                    else:
-                        circuits[cid] = [(timestamp, event)]
-
-        # filter out circuits with blacklisted addresses
-        for cid in list(circuits.keys()):
-            if blacklist_hit(circuits[cid]):
-                del circuits[cid]
-        # filter out circuits with only IPs (unless arg says otherwise)
-        for cid in list(circuits.keys()):
-            if not args["ip"] and only_ips(circuits[cid]):
-                del circuits[cid]
+        circuits = common.circpad_extract_log_traces(infname, args["ip"])
 
         if len(circuits) == 0:
-            sys.exit(f"no circuits left after filtering for {infname}")
-
-        # remove blacklisted events (and associated events)
-        for cid in list(circuits.keys()):
-            circuits[cid] = remove_blacklisted_events(circuits[cid])
+            sys.exit(f"no valid circuits found for {infname}")
 
         # figure out which circuit has the most events, keeping tabs of the rest
         longest_cid = -1
@@ -106,80 +73,6 @@ def main():
         for cid in smaller_cids:
             if len(circuits[cid]) > CONST_OTHER_MAX_EVENTS_WARNING:
                 print(f"warning: found extra circuit with {len(circuits[cid])} events in {infname}")
-
-def remove_blacklisted_events(trace):
-    # three cases: no argument, filter client, or filter relay
-    result = []
-    ignore_next_events = 0
-
-    for line in trace:
-        # we always filter blacklisted events
-        if not any(b in line for b in blacklisted_events):
-            if ignore_next_events == 0:
-                result.append(line)
-            else:
-                ignore_next_events -= 1
-
-    '''
-    For the client, we assume a list as follows:
-      1. circpad_negotiate_padding
-      2. circpad_cell_event_nonpadding_sent
-      3. circpad_cell_event_nonpadding_received
-      4. circpad_handle_padding_negotiated
-    On observing (blacklisted) event 1, we ignore the following 2 events
-    '''
-    if args["fnc"] and "circpad_negotiate_padding" in line:
-        ignore_next_events = 2
-
-    '''
-    For the relay, we assume a list as followes:
-      1. circpad_handle_padding_negotiate
-      2. circpad_padding_negotiated
-      3. circpad_cell_event_nonpadding_sent
-    On observing (blacklisted) event 2, we ignore the following 1 event
-    '''
-    if args["fnr"] and "circpad_padding_negotiated" in line:
-        ignore_next_events = 1
-
-    return result
-
-def extract_trace(line):
-    n = line.index(CONST_CIRCPAD_TRACE_TIMESTAMP)+len(CONST_CIRCPAD_TRACE_TIMESTAMP)
-    timestamp = line[n:].split(" ", maxsplit=1)[0]
-    n = line.index(CONST_CIRCPAD_TRACE_CIRC_ID)+len(CONST_CIRCPAD_TRACE_CIRC_ID)
-    cid = line[n:].split(" ", maxsplit=1)[0]
-
-    # an event is the last part, no need to split on space like we did earlier
-    n = line.index(CONST_CIRCPAD_TRACE_EVENT)+len(CONST_CIRCPAD_TRACE_EVENT)
-    event = line[n:]
-    
-    return cid, timestamp, event
-
-def blacklist_hit(d):
-    for a in common.circpad_get_all_addresses(d):
-        if a in blacklisted_addresses:
-            return True
-    return False
-
-def only_ips(d):
-    def is_ipv4(addr):
-        try:
-            socket.inet_aton(addr)
-        except (socket.error, TypeError):
-            return False
-        return True
-    def is_ipv6(addr):
-        try:
-            socket.inet_pton(addr,socket.AF_INET6)
-        except (socket.error, TypeError):
-            return False
-        return True
-
-    for a in common.circpad_get_all_addresses(d):
-        if not is_ipv4(a) and not is_ipv6(a):
-            return False
-    return True
-
 
 if __name__ == "__main__":
     main()
